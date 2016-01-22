176884e67fb0368d9ff67fe157daca39a81579a5
Chien Huey <chien.huey@aenetworks.com>
2016-01-21 22:13:22 -0500

    Used boto3 libraries to take advantage of aws_cli profiles for credentials. TODO: parse vcl to get ELB DNS - this because Fastly doesn't return backend (that's defined in .vcl) information through the API

--- which_stack_is_live.py
+++ which_stack_is_live.py
@@ -5,6 +5,8 @@
 # @param    aws_cli profile name
 # @param    property name in Fastly
 # @param    Fastly API key
+# @param    Fastly username (optional)
+# @param    Fastly password (optional)
 
 import boto3
 import fastly
@@ -12,23 +14,23 @@ import sys
 
 __author__ = 'chhuey'
 
-if len(sys.argv) != 4:
+if len(sys.argv) < 4:
     print "usage which_stack_is_live <aws_cli profile name> <property name in Fastly> <Fastly API key>"
     sys.exit(0)
 else:
     fastlyAPIKey = sys.argv[3]
     awscliProfile = sys.argv[1]
     propertyName = sys.argv[2]
+    fastlyUserId = sys.argv[4]
+    fastlyPassword = sys.argv[5]
 
 
 awsSession = boto3.session.Session(awscliProfile)
 
 client = fastly.connect(fastlyAPIKey)
+client.login(fastlyUserId, fastlyPassword)
 propertyShareId = ''
 lastVersion = 0
-
-# get all the services on Fastly
-allServices = client.list_services()
 
 # determine the Fastly shareID of the property
 service = client.get_service_by_name(propertyName)
@@ -36,4 +38,7 @@ versionList = service._data['versions']
 lastVersion = int(versionList[-1]['number'])
 propertyShareId = versionList[-1]['service']
 
+# list all the backends associated with the service
+backends = client.list_backends(propertyShareId, lastVersion)
 
+print client.get_backend(propertyShareId, lastVersion, "aetv_com")
