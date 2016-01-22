911d55a82cbc8507e989a8fe370794745f4e409c
Chien Huey <chien.huey@aenetworks.com>
2016-01-22 00:00:24 -0500

    Updated README.md. Also made progress in parsing the vcl. TODO: finish parsing. Refactor functions into separate module.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # aws_tools
 Tools to help with AWS. Various scripts to make life with AWS a little easier - mostly using Python.
-- which_stack_is_live a script to determine given the name of a property (as named in Fastly) which Cloud Formation
+- which_stack_is_live.py - a script to determine given the name of a property (as named in Fastly) which Cloud Formation
     stack is the active stack. Helpful in environments where blue/green deployments are performed with two or more
     Cloud Formation stacks
 
--- which_stack_is_live.py
+++ which_stack_is_live.py
@@ -10,9 +10,33 @@
 
 import boto3
 import fastly
+import re
 import sys
 
 __author__ = 'chhuey'
+
+def get_active_version(versions):
+    active_version_number = 0
+    index = len(versions) - 1
+    while index >= 0:
+        if versions[index]['active'] == '1':
+            active_version_number = int(versions[index]['number'])
+            break
+        else:
+            index -= 1
+    return active_version_number
+
+def parse_vcl(active_version):
+    vcl_list = active_version['vcl']
+    backend_vcl = ""
+    # find the vcl with backend information
+    for vcl in vcl_list:
+        if re.match('backend', vcl['content']):
+            backend_vcl = vcl['content']
+            break
+
+    print backend_vcl
+
 
 if len(sys.argv) < 4:
     print "usage which_stack_is_live <aws_cli profile name> <property name in Fastly> <Fastly API key>"
@@ -29,16 +53,26 @@ awsSession = boto3.session.Session(awscliProfile)
 
 client = fastly.connect(fastlyAPIKey)
 client.login(fastlyUserId, fastlyPassword)
-propertyShareId = ''
+propertyshareid = ""
 lastVersion = 0
 
 # determine the Fastly shareID of the property
 service = client.get_service_by_name(propertyName)
 versionList = service._data['versions']
-lastVersion = int(versionList[-1]['number'])
-propertyShareId = versionList[-1]['service']
+activeVersion = get_active_version(versionList)
+propertyshareid = versionList[-1]['service']
 
 # list all the backends associated with the service
-backends = client.list_backends(propertyShareId, lastVersion)
+backends = client.list_backends(propertyshareid, activeVersion)
 
-print client.get_backend(propertyShareId, lastVersion, "aetv_com")
+if len(backends) == 0:
+    version_to_go = activeVersion - 1
+    active_version = versionList[version_to_go]
+    parse_vcl(active_version)
+
+
+
+
+
+
+
